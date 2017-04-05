43c6d67b0bd7bf3ebf225ae6a18302559bded7b6
Jacob Adams <tookmund@gmail.com>
2017-04-05 17:29:36 -0400

    gobot: Get homeserver from password file as well

--- gobot.go
+++ gobot.go
@@ -7,8 +7,8 @@ import (
 )
 var username string
 
-func Login(homeserver string) *gomatrix.Client {
-	user, pass := getsecret()
+func Login() *gomatrix.Client {
+	user, pass, homeserver := getsecret()
 	cli, _ := gomatrix.NewClient(homeserver, "", "")
 	resp, err := cli.Login(&gomatrix.ReqLogin{
 		Type: "m.login.password",
@@ -31,13 +31,13 @@ func sync(cli *gomatrix.Client) {
 	}
 }
 
-func getsecret() (user, pass string) {
+func getsecret() (user, pass, homeserver string) {
 	file, err := os.Open("password")
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(0)
 	}
-	fmt.Fscan(file, &user, &pass)
+	fmt.Fscan(file, &user, &pass, &homeserver)
 	return 
 }
 
